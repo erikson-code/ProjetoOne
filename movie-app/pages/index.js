4890299df6499780fe54d5cd76b3b56330f1a072
erikson-code <erikson91249650@gmail.com>
2021-04-01 16:52:58 -0300

    Adding data

--- movie-app/pages/index.js
+++ movie-app/pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 
 import NavBar from '../components/navbar.js'
 import Footer from '../components/Footer'
@@ -6,8 +5,11 @@ import ResourcesList from '../components/ResourseList'
 import ResourcesHigh from '../components/ResourseHighLight'
 import Newsletter from '../components/Newsletter'
 
+import data from './api/data'
+
 export default function Home() {
-  
+
+  console.log(data)
 
   return (
     <div>
