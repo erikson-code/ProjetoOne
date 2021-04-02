13f5425bdc53b7d6229c7f3e149f2d465c59bc41
erikson-code <erikson91249650@gmail.com>
2021-04-02 09:12:19 -0300

    Server criated

--- content-movie-manager-app/index.js
+++ content-movie-manager-app/index.js
@@ -1,9 +1,24 @@
+
 const express = require("express")
-
 const app = express()
-
 const PORT = 3001
 
-app.listen(PORT,()=>{
-    console.log("Server is listening on port:"+PORT)
+const fs = require("fs")
+const path = require("path")
+const pathToFile = path.resolve("./data.json")
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Word")
+
+})
+app.get("/api/resources", (req, res) => {
+    const stringifiedData = fs.readFileSync(pathToFile)
+    const Resources = JSON.parse(stringifiedData)
+    res.send(Resources)
+
+})
+
+app.listen(PORT, () => {
+    console.log("Server is listening on port:" + PORT)
 })
